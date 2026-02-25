444127fad59dbafbce77bb3d9a57fbfbd38da1d0
Andhika Daffa Athaaillah <andhikadaffaathaaillah@gmail.com>
2026-02-25 10:17:07 +0700

    jobsheet 2 practicum 2

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/hello', function () {
-return 'Hello World';
-});
-Route::get('/world', function () {
-return 'World';
-});
-Route::get('/welcome', function () {
-return 'Welcome';
-});
-Route::get('/NIM&name', function () {
-return '244107020223 - Andhika Daffa Athaaillah';
-});
-Route::get('/user/{name?}', function ($name='John') {
-    return 'My name is Daffa '.$name;
-});
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-return 'Post to '.$postId.' Comments to: '.$commentId;
-});
-Route::get('/articles/{id}', function ($id) {
-    return "Article Page with ID " . $id;
-});
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+Route::resource('photos', PhotoController::class);
